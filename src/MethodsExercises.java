7ccc8282f86e5efde47e2ef3d8759199496c3b94
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-02 15:35:34 -0500

    feat: saving progress on exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,8 +3,23 @@ public class MethodsExercises {
         System.out.println(firstOperand + secondOperand);
         return firstOperand + secondOperand;
     }
+    public static int subtraction(int firstOperand, int secondOperand){
+        System.out.println(firstOperand - secondOperand);
+        return firstOperand - secondOperand;
+    }
+    public static int multiplication(int firstOperand, int secondOperand){
+        System.out.println(firstOperand * secondOperand);
+        return firstOperand * secondOperand;
+    }
+    public static int division(int firstOperand, int secondOperand){
+        System.out.println(firstOperand / secondOperand);
+        return firstOperand / secondOperand;
+    }
 
     public static void main(String[] args) {
         addition(4, 2);
+        subtraction(4, 2);
+        multiplication(4, 2);
+        division(4, 2);
     }
 }
